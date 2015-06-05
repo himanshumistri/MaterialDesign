da62a1e129ea484b4a470ff5ad51339ae374b516
Himanshu4003 <mistrihimanshu@gmail.com>
2015-06-05 12:32:29 +0530

    Added new SectionAdapter class.

---
+++ app/src/main/java/com/himotech/matrialdesign/HomeSectionAdapter.java
@@ -0,0 +1,9 @@
+package com.himotech.matrialdesign;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by ubbvand2 on 5/6/15.
+ */
+public class HomeSectionAdapter extends RecyclerView.Adapter{
+}
